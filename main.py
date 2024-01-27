e5c2aa3b36bd9f18f73dcfe3a9c26833d5be06ac
Gaurav Chauhan <gaurav.chauhan911@gmail.com>
2024-01-27 18:07:06 -0500

    Added test apis
    root and printname APIs added to test the setup

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-from fastapi import fastapi
+#####
+# This main file acts as a controller for APIs
+#####
+
+from fastapi import FastAPI
+
+global app
+app = FastAPI()
+
+
+@app.get("/")
+def getIndex():
+    return "FastAPI server is up"
+
+
+@app.get("/printName")
+def printName(name: str):
+    return 'Hello,' + name
